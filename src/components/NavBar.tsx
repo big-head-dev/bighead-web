6ca7b4139b3adda13d28c223b959fb09b1c16feb
big-head-dev <32729675+big-head-dev@users.noreply.github.com>
2025-03-13 23:12:16 +0700

    navbar bg scroll effect

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { cn } from '../utils/cn'
 import bhLogo from '/logo-white.svg'
 
@@ -5,9 +6,28 @@ type Props = {
   className?: string
   children?: React.ReactNode
 }
-export const NavBar = ({ className, children, ...props }: Props) => (
-  <nav className={cn('inline-flex gap-4 px-5 md:px-10 md:py-5 py-2 h-sm w-full', className)} {...props}>
-    <img fetchPriority="high" src={bhLogo} alt="bighead development" className="w-30 md:w-50" />
-    {children}
-  </nav>
-)
+export const NavBar = ({ className, children, ...props }: Props) => {
+  const [scrolled, setScrolled] = useState(false)
+
+  const handleScroll = () => setScrolled(window.scrollY > 0)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <nav
+      className={cn(
+        'inline-flex gap-4 px-5 md:px-10 md:py-5 py-2 h-sm w-full transition-colors duration-300 ease-in',
+        scrolled ? 'bg-gradient-to-b from-bh-dblue to-bh-dblue/70 hover:to-bh-dblue/90' : 'bg-transparent',
+        className
+      )}
+      {...props}>
+      <img fetchPriority="high" src={bhLogo} alt="bighead development" className="w-30 md:w-50" />
+      {children}
+    </nav>
+  )
+}

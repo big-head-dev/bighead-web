7866b84b624513e1637599bc75ca0300b88fb69c
big-head-dev <32729675+big-head-dev@users.noreply.github.com>
2025-03-21 06:15:22 +0700

    moved some data to constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const techStack: string[] = ['C#', 'TS/JS', 'CSS', 'Go', 'Java', 'C']
+
+export const stockSymbols: string[] = [
+  'AAPL',
+  'TSLA',
+  'GOOG',
+  'MSFT',
+  'AMZN',
+  'META',
+  'NFLX',
+  'NVDA',
+  'INTC',
+  'AMD',
+  'BA',
+  'F',
+  'T',
+  'XOM',
+  'GE',
+  'HOV',
+]

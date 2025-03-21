82a662795d0106d460280dcf0922f81af77a541f
big-head-dev <32729675+big-head-dev@users.noreply.github.com>
2025-03-21 07:52:09 +0700

    added github

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { cn } from '../utils/cn'
 import bhLogo from '/logo-white.svg'
+import githubLogo from '/github-mark.svg'
 
 type Props = {
   className?: string
@@ -21,13 +22,20 @@ export const NavBar = ({ className, children, ...props }: Props) => {
   return (
     <nav
       className={cn(
-        'inline-flex gap-4 px-5 md:px-10 md:py-5 py-2 h-sm w-full transition-colors duration-300 ease-in',
+        'flex flex-row items-center gap-4 px-5 md:px-10 md:py-5 py-2 h-sm w-full transition-colors duration-300 ease-in',
         scrolled ? 'bg-gradient-to-b from-bh-dblue to-bh-dblue/70 hover:to-bh-dblue/90' : 'bg-transparent',
         className
       )}
       {...props}>
       <img fetchPriority="high" src={bhLogo} alt="bighead development" className="w-30 md:w-50" />
       {children}
+      <ul className="ml-auto flex flex-row items-center">
+        <li>
+          <a href="http://github.com/big-head-dev" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="bighead's github" className="w-6 md:w-8" />
+          </a>
+        </li>
+      </ul>
     </nav>
   )
 }
